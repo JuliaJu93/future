ca8739eb3c058044cd82150d1b03cfce510b761b
Julia Yusupova <cashleva.iulia@gmail.com>
2020-09-11 01:00:47 +0300

    added add button

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import DataSelection from './components/data_selection/data_selection.js';
+import AddButton from './components/add_button/add_button.js';
 
 function Main() {
   return (
     <main>
         <DataSelection />
+        <AddButton />
     </main>
   );
 }
