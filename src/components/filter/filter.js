da5fad0e411b6a942e5819de824f9f8a4248d0e0
Julia Yusupova <cashleva.iulia@gmail.com>
2020-09-11 01:05:08 +0300

    Added component filter

---
+++ src/components/filter/filter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import FindButton from '../find_button/find_button.js';
+
+function Filter() {
+  return (
+    <div>
+        <input></input>
+        <FindButton />
+    </div>
+  );
+}
+
+export default Filter;
