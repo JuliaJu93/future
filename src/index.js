0862c3928af6af8205bb69c163f0f305951c64e0
Julia Yusupova <cashleva.iulia@gmail.com>
2020-09-11 00:23:12 +0300

    drew up the project structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+function Main() {
+  return (
+    <main>
+
+    </main>
+  );
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
