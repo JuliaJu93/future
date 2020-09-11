8b8dc2e17ef5e9b0287464f900585864a625dcea
Julia Yusupova <cashleva.iulia@gmail.com>
2020-09-12 00:40:28 +0300

    Adde prettier

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-import {showButtonOfForm} from './show_button_of_form';
-import {addInTable} from './add_in_table';
+import { showButtonOfForm } from './show_button_of_form';
+import { addInTable } from './add_in_table';
 
-import './form.css'
+import './form.css';
 
 function Form() {
   const [id, setId] = useState('');
@@ -37,68 +37,64 @@ function Form() {
     }
   }
 
-  function handleSubmit (event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    addInTable ();
+    addInTable();
   }
 
   return (
     <form className="addForm" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="id"> id: </label>
+        <input type="text" id="id" name="Id" required onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="firstName"> firts name: </label>
+        <input
+          type="text"
+          id="firstName"
+          name="FirstName"
+          required
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="lastName"> last name: </label>
+        <input
+          type="text"
+          id="lastName"
+          name="LastName"
+          required
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="email"> email: </label>
+        <input
+          type="email"
+          id="email"
+          name="Email"
+          required
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="phone"> phone: </label>
+        <input
+          type="phone"
+          id="phone"
+          name="Phone"
+          required
+          onChange={handleChange}
+        />
+      </div>
+      {showButtonAdd && (
         <div>
-          <label htmlFor="id"> id: </label>
-          <input
-            type="text"
-            id="id"
-            name="Id"
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="firstName"> firts name: </label>
-          <input
-            type="text"
-            id="firstName"
-            name="FirstName"
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="lastName"> last name: </label>
-          <input
-            type="text"
-            id="lastName"
-            name="LastName"
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="email"> email: </label>
-          <input
-            type="email"
-            id="email"
-            name="Email"
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="phone"> phone: </label>
-          <input
-            type="phone"
-            id="phone"
-            name="Phone"
-            required
-            onChange={handleChange}
-          />
-        </div>
-        {showButtonAdd && <div>
           <input type="submit" value="Добавить в таблицу" />
-        </div>}
-      </form>
-    );
+        </div>
+      )}
+    </form>
+  );
 }
 
 export default Form;
