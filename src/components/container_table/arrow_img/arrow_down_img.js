2af2cf16788cbd262849387fb9812ff73dbc77db
Julia Yusupova <cashleva.iulia@gmail.com>
2020-09-12 21:49:39 +0300

    Added img arrows

---
+++ src/components/container_table/arrow_img/arrow_down_img.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import arrow_down from '../../../img/arrow_down.png';
+
+function ArrowDownImg() {
+  
+  return (
+    <img src={arrow_down} alt = 'arrow down'></img>
+  );
+}
+
+export default ArrowDownImg;
--- src/components/container_table/table/table_head.js
+++ src/components/container_table/table/table_head.js
@@ -1,23 +1,36 @@
 import React from 'react';
 
+import ArrowUpImg from '../arrow_img/arrow_up_img';
+import ArrowDownImg from '../arrow_img/arrow_down_img';
+
 function TableHead() {
   return (
         <thead>
             <tr>
                 <th>
                     id
+                    <ArrowUpImg />
+                    <ArrowDownImg />
                 </th>
                 <th>
                     first name
+                    <ArrowUpImg />
+                    <ArrowDownImg />
                 </th>
                 <th>
                     last name
+                    <ArrowUpImg />
+                    <ArrowDownImg />
                 </th>
                 <th>
                     email
+                    <ArrowUpImg />
+                    <ArrowDownImg />
                 </th>
                 <th>
                     phone
+                    <ArrowUpImg />
+                    <ArrowDownImg />
                 </th>
             </tr>
         </thead>
